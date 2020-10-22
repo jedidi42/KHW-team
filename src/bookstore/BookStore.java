d401a994c1d302bc02723358b94da0318159e23c
hamza <jedidi42@gmail.com>
2020-10-22 11:56:40 +0100

    khw version 0

---
+++ src/bookstore/BookStore.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bookstore;
+
+/**
+ *
+ * @author chaym
+ */
+public class BookStore {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Welcome to Our Book Store");
+    }
+    
+}
---
+++ src/bookstore/Models/Categorie.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bookstore.Models;
+
+/**
+ *
+ * @author chaym
+ */
+public class Categorie {
+    String name;
+}
