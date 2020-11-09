c6b999d93702fef04cbb46c6eed978cf7ef065d8
hamza <jedidi42@gmail.com>
2020-11-09 15:38:21 +0100

    step2 class book

--- src/entities/Book.java
+++ src/entities/Book.java
@@ -7,6 +7,42 @@ int id ;
 String title ; 
 String author ;
 double price ; 
-Date releaseDate ; 	
+Date releaseDate ;
+
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getTitle() {
+	return title;
+}
+public void setTitle(String title) {
+	this.title = title;
+}
+public String getAuthor() {
+	return author;
+}
+public void setAuthor(String author) {
+	this.author = author;
+}
+public double getPrice() {
+	return price;
+}
+public void setPrice(double price) {
+	this.price = price;
+}
+public Date getReleaseDate() {
+	return releaseDate;
+}
+public void setReleaseDate(Date releaseDate) {
+	this.releaseDate = releaseDate;
+}
+@Override
+public String toString() {
+	return "Book [id=" + id + ", title=" + title + ", author=" + author + ", price=" + price + ", releaseDate="
+			+ releaseDate + "]";
+} 	
 
 }
