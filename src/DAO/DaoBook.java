438d112d4369620c039bfe7c96c2460a39b38caa
koussay bilel <koussaybilel02@gmail.com>
2020-11-09 15:29:32 +0100

    step2

--- src/DAO/DaoBook.java
+++ src/DAO/DaoBook.java
@@ -1,6 +1,15 @@
 package DAO;
 
+import java.util.List;
+
+import entities.Book;
+
 public class DaoBook {
 
+	void addBook (Book book) {}
 	
+	 List<Book>listBook() {
+		 
+		 return (listBook()) ;
+	 }
 }
