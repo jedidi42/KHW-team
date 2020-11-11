e1a43a7682b90bccaeb727e686767793e02fc2a1
wael khannassa <wkhannassa@gmail.com>
2020-11-11 13:08:33 +0100

    version final

--- src/gui/welcome.java
+++ src/gui/welcome.java
@@ -391,7 +391,9 @@ public void show_books() throws SQLException{
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
+         
         try {
+              
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
@@ -408,13 +410,13 @@ public void show_books() throws SQLException{
             java.util.logging.Logger.getLogger(welcome.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-
+ 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 try {
                     new welcome().setVisible(true);
-  
+
                 } catch (SQLException ex) {
                     Logger.getLogger(welcome.class.getName()).log(Level.SEVERE, null, ex);
                 }
